dab9ae52d9bf5a05343de584ff5d24fcfacc493f
ehabashrafali <Opaa0100@>
2025-07-19 01:37:41 +0300

    modify controllers

--- CaseStudy.Host/Program.cs
+++ CaseStudy.Host/Program.cs
@@ -1,63 +1,59 @@
-
 using CaseStudy.Application.Commands.CreateEmployee;
 using CaseStudy.Application.Queries.Repositories;
 using CaseStudy.Domain.Repositories;
+using CaseStudy.Infrastructure.API.Controllers;
 using CaseStudy.Infrastructure.DataBase;
 using CaseStudy.Infrastructure.DataBase.Departments.Repositories;
 using CaseStudy.Infrastructure.DataBase.Employees.Repositories;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging.Debug;
 
-namespace CaseStudy.Host
+var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
+builder.Services.AddDbContext<CaseStudyDbContext>(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+    options.UseSqlServer(connectionString);
+    options.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddDebug()));
+});
 
+builder.Services.AddScoped<IDepartmentCommandRepository, DepartmentCommandsRepository>();
+builder.Services.AddScoped<IEmployeeCommandRepository, EmployeeCommandRepository>();
+builder.Services.AddScoped<IDepartmentQueriesRepository, DepartmentQueriesRepository>();
+builder.Services.AddScoped<IEmployeeQueriesRepository, EmployeeQueriesRepository>();
 
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddTransient<IPipelineBehavior<CreateEmployeeCommand, Guid>, CreateEmployeeCommandPreparerBehavior>();
 
-            builder.Services.AddDbContext<CaseStudyDbContext>(options =>
-            {
-                var serviceProvider = builder.Services.BuildServiceProvider();
-                var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
-                loggerFactory.AddProvider(new DebugLoggerProvider());
-                options.UseSqlServer(connectionString);
-            });
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssemblyContaining<CreateEmployeeCommand>();
+});
 
-            builder.Services.AddScoped<IDepartmentCommandRepository, DepartmentCommandsRepository>();
-            builder.Services.AddScoped<IEmployeeCommandRepository, EmployeeCommandRepository>();
-            builder.Services.AddScoped<IDepartmentQueriesRepository, DepartmentQueriesRepository>();
-            builder.Services.AddScoped<IEmployeeQueriesRepository, EmployeeQueriesRepository>();
+builder.Services
+    .AddControllers()
+    .AddApplicationPart(typeof(DepartmentController).Assembly)
+    .AddApplicationPart(typeof(EmployeeController).Assembly);
 
-            builder.Services.AddTransient<IPipelineBehavior<CreateEmployeeCommand, Guid>, CreateEmployeeCommandPreparerBehavior>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.CustomSchemaIds(type => type.FullName);
+});
 
-            // Add services to the container.
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+var app = builder.Build();
 
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
-
-            app.Run();
-        }
-    }
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
--- CaseStudy.Infrastucture.API/Controllers/EmployeeController.cs
+++ CaseStudy.Infrastucture.API/Controllers/EmployeeController.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace CaseStudy.Infrastructure.API.Controllers
 {
     [ApiController]
-    [Route("api/employees/")]
+    [Route("api/employees")]
     public class EmployeeController(ISender sender) : ControllerBase
     {
         [HttpPost]
@@ -27,7 +27,7 @@ namespace CaseStudy.Infrastructure.API.Controllers
         }
 
 
-        [HttpGet("{id}")]
+        [HttpGet("delete/{id}")]
         public async Task<IActionResult> DeleteEmployee(Guid id, [FromQuery] Guid version, CancellationToken cancellationToken)
         {
             var command = new DeleteEmployeeCommand(id, version);
