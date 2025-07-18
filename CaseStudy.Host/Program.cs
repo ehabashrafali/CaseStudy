78ef284d9d3302d8feeccb356d23bec1d3cafb9c
ehabashrafali <Opaa0100@>
2025-07-18 12:51:17 +0300

    adding connection string

--- CaseStudy.Host/Program.cs
+++ CaseStudy.Host/Program.cs
@@ -1,4 +1,8 @@
 
+using CaseStudy.Infrastructure.DataBase;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Debug;
+
 namespace CaseStudy.Host
 {
     public class Program
@@ -7,8 +11,18 @@ namespace CaseStudy.Host
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
 
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+           
+            builder.Services.AddDbContext<CaseStudyDbContext>(options =>
+            {
+                var serviceProvider = builder.Services.BuildServiceProvider();
+                var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+                loggerFactory.AddProvider(new DebugLoggerProvider());
+                options.UseSqlServer(connectionString);
+            });
+
+            // Add services to the container.
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
